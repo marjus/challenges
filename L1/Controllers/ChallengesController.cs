74aeb759c8fb1224e093177d02e95d39fc637f66
Marjus Nielsen <marjus.nielsen@regionblekinge.se>
2022-05-17 19:23:22 +0200

    changed texts to english

--- L1/Controllers/ChallengesController.cs
+++ L1/Controllers/ChallengesController.cs
@@ -18,30 +18,30 @@ namespace L1.Controllers
             challenges.ActiveChallenge = new Challenge
             {
                 Id = 211,
-                Name = "Enda setning",
-                Text = "Grasid er __",
-                Question = "Hvönn lit er grasid?",
+                Name = "Complete the sentence",
+                Text = "The grass is __",
+                Question = "What color is the grass?",
                 CorrectOptionId = 1,
                 Options = new List<ChallengeOption>() { 
-                    new ChallengeOption { Id = 1, Content = "Grönt" }, 
-                    new ChallengeOption { Id = 2, Content = "Reytt" },
-                    new ChallengeOption { Id = 3, Content = "Blátt" },
-                    new ChallengeOption { Id = 4, Content = "Svart" },
+                    new ChallengeOption { Id = 1, Content = "Green" }, 
+                    new ChallengeOption { Id = 2, Content = "Red" },
+                    new ChallengeOption { Id = 3, Content = "Blue" },
+                    new ChallengeOption { Id = 4, Content = "Black" },
                 }
             };
 
             challenges.Challenges.Add(new Challenge
             {
                 Id = 1,
-                Name = "Enda setning",
-                Text = "Vatnid er __",
-                Question = "Hvat er vatn?",
+                Name = "Complete the sentence",
+                Text = "Water is __",
+                Question = "Water is what??",
                 CorrectOptionId = 3,
                 Options = new List<ChallengeOption>() {
-                    new ChallengeOption { Id = 1, Content = "Livandi" },
-                    new ChallengeOption { Id = 2, Content = "Irriterandi" },
-                    new ChallengeOption { Id = 3, Content = "Vátt" },
-                    new ChallengeOption { Id = 4, Content = "Grönt" },
+                    new ChallengeOption { Id = 1, Content = "Alive" },
+                    new ChallengeOption { Id = 2, Content = "Annoying" },
+                    new ChallengeOption { Id = 3, Content = "Wet" },
+                    new ChallengeOption { Id = 4, Content = "Green" },
                 }
             });
 
