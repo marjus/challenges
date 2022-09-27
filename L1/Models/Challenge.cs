0ade9336c11b4767293fd4967f1501ef4c877a85
marjus <marjus@hotmail.com>
2022-09-27 10:43:58 +0200

    Create and edit pages complete for v1

--- L1/Models/Challenge.cs
+++ L1/Models/Challenge.cs
@@ -7,30 +7,38 @@ namespace L1.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Nummar")]
         public int OrderInSequence { get; set; }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        [Display(Name = "Intro")]
         public string? Name { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
+        [Display(Name = "Tekstur")]
         public string? Text { get; set; }
 
+        [Display(Name = "Frágreiding")]
         public string? Description { get; set; }
-
+        [Display(Name = "Spurningur")]
         public string? Question { get; set; }
 
+        [Display(Name = "Slag")]
         public int TypeId { get; set; }
 
+        [Display(Name = "Slag")]
         public ChallengeType Type { get; set; }
 
         public int DifficultyLevelId { get; set; }
 
+        [Display(Name = "Torleikastig")]
         public ChallengeDifficultyLevel DifficultyLevel { get; set; }
 
+        [Display(Name = "Legg út til öll")]
         public bool IsOpenForAll { get; set; }
 
         public int CorrectOptionId { get; set; }
 
+        [Display(Name = "Svarmøguleikar")]
         public List<ChallengeOption> Options { get; set; }
     }
 }
