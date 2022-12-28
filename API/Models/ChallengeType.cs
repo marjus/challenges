d2fb50c5bc8470f8dd076926ed459230dad3a8ff
marjus <marjus@hotmail.com>
2022-12-28 14:55:38 +0100

    added api project with default scaffolded challenges controller

--- API/Models/ChallengeType.cs
+++ API/Models/ChallengeType.cs
@@ -5,15 +5,16 @@ namespace API.Models
 {
     public enum ChallengeType
     {
-
+        [Display(Name = "Enda setning")]
         CompleteSentence = 1,
 
-      
+        [Display(Name = "Vel rætt orð")]
         TextAndQtoWord = 2,
 
+        [Display(Name = "Matcha orð")]
         MatchWords = 3,
 
-
+        [Display(Name = "Vel røttu mynd")]
         TextAndQtoImage = 4 
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,12 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,11 +16,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
