cadb2d329886124dbe2c016c2056084e680a24ea
marjus <marjus@hotmail.com>
2022-12-30 21:01:48 +0000

    added navbar

--- front/challenges.GUI/src/App.vue
+++ front/challenges.GUI/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <header>
-    <div class="wrapper">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <nav>
-        <RouterLink to="/challenges">Umsiting</RouterLink>
-        <RouterLink to="/run">Royn uppgávur</RouterLink>
-      </nav>
-    </div>
-  </header>
-  
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/challenges">Umsiting</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/run">Royn uppgávur</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <RouterView />
   
 </template>
@@ -30,31 +37,6 @@ header {
 }
 
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
@@ -72,13 +54,6 @@ nav a:first-of-type {
     flex-wrap: wrap;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
