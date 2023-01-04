5019bdc925660f11383df5d8e59464a27294838b
marjus <marjus@hotmail.com>
2023-01-04 14:05:08 +0000

    removed navbar collapse

--- front/challenges.GUI/src/App.vue
+++ front/challenges.GUI/src/App.vue
@@ -1,10 +1,6 @@
 <template>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
 
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item">
             <RouterLink class="nav-link" to="/">Umsiting</RouterLink>
@@ -13,7 +9,7 @@
             <RouterLink class="nav-link" to="/run">Royn uppgávur</RouterLink>
           </li>
         </ul>
-      </div>
+
     </nav>
 
     <RouterView />
